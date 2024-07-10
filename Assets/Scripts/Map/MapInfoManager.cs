f946b08c87f0fdd556150e4669a3ade21cc69cbe
flashcy <140628455+flashcy@users.noreply.github.com>
2024-07-10 12:06:05 +0900

    feat/connects Web API for the map scene (#4)

---
+++ Assets/Scripts/Map/MapInfoManager.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+
+[System.Serializable]
+public class StageData
+{
+    public string userId;
+    public string theme;
+    public int stage;
+    public int correctAnswers;
+}
+
+[System.Serializable]
+public class StageResponseData
+{
+    public int status;
+    public bool success;
+    public string message;
+    public StageData[] data;
+}
+
+
+public class MapInfoManager : MonoBehaviour
+{
+    [SerializeField]
+    private string userId;
+    [SerializeField]
+    private string theme;
+
+    // 텍스트
+    [SerializeField]
+    private TextMeshProUGUI textCorrectness;
+    [SerializeField]
+    private TextMeshProUGUI textStageInfo;
+    [SerializeField]
+    private TextMeshProUGUI textNoInfo;
+
+    // 텍스트 오브젝트
+    [SerializeField]
+    private GameObject objCorrectness;
+    [SerializeField]
+    private GameObject objStageInfo;
+    [SerializeField]
+    private GameObject objNoInfo;
+
+    // 별과 관련된 변수들
+    [SerializeField]
+    private SpriteRenderer[] spriteRenderer = new SpriteRenderer[3];
+    [SerializeField]
+    private Sprite yellowStar;
+
+    private StageData[] stageData;
+    private string url;
+
+    void Start()
+    {
+        url = $"https://worderland.kro.kr/api/result/{userId}?theme={theme}";
+        StartCoroutine(GetRequest(url));
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(uri))
+        {
+            // 요청 보내기
+            yield return request.SendWebRequest();
+
+            // 오류 처리
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError(request.error);
+            }
+            else
+            {
+                // 응답 받기
+                string jsonResponse = request.downloadHandler.text;
+                Debug.Log("Response: " + jsonResponse);
+
+                // JSON 파싱
+                StageResponseData responseData = JsonUtility.FromJson<StageResponseData>(jsonResponse);
+
+                // 데이터 접근
+                if (responseData.success)
+                {
+                    if (responseData.data == null)
+                    {
+                        // 빈 배열이면 유저 정보 없는 것
+                        // 따라서 "기록 없음"으로 출력
+                        objCorrectness.SetActive(false);
+                        objStageInfo.SetActive(false);
+                        objNoInfo.SetActive(true);
+                        textNoInfo.text = "기록 없음";
+                    }
+                    else
+                    {
+                        stageData = responseData.data.Clone() as StageData[];
+                        foreach (var userData in responseData.data)
+                        {
+                            Debug.Log("User ID: " + userData.userId);
+                            Debug.Log("Theme: " + userData.theme);
+                            Debug.Log("Stage: " + userData.stage);
+                            Debug.Log("Correct Answers: " + userData.correctAnswers);
+                        }
+                        int correct = 0;
+                        for(int i = 0; i < stageData.Length - 1; i++)
+                        {
+                            correct += stageData[i].correctAnswers;
+                        }
+                        // UI 업데이트
+                        objCorrectness.SetActive(true);
+                        objStageInfo.SetActive(true);
+                        objNoInfo.SetActive(false);
+
+                        float correctness = Mathf.Floor(correct / 11 * 100);
+                        textCorrectness.text = "정답률";
+                        textStageInfo.text = $"{correctness}%";
+                        // 별 UI 업데이트
+                        if (correctness > 0f)
+                            spriteRenderer[0].sprite = yellowStar;
+                        if (correctness > 50f)
+                            spriteRenderer[1].sprite = yellowStar;
+                        if (correct == 11)
+                            spriteRenderer[2].sprite = yellowStar;
+                    }
+                }
+                else
+                {
+                    Debug.LogError("Request failed with message: " + responseData.message);
+                }
+            }
+        }
+    }
+}
